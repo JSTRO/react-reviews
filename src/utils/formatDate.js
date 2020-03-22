79bec491be1d040987577361af606db31f659e7d
Joseph Ostrow <jstro@umich.edu>
2020-03-21 21:29:16 -0400

    modify data format

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,5 @@
 export default function formatDate(date) {
-	const dateOptions = {year: 'numeric', month: 'short', day: 'numeric'}
+	const dateOptions = {year: 'numeric', month: 'long', day: 'numeric'}
 	const newDate = new Date(date)
-	return newDate.toLocaleDateString("en-US", dateOptions)
+	return newDate.toLocaleDateString("en-US", dateOptions).replace(',', '')
 }
